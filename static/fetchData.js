14a30b555c53f39e08ff846b8c33b5728b4c6b0f
Faustas V <55922917+prOcenTas@users.noreply.github.com>
2022-09-29 10:25:44 +0200

    Using new method + module objects

--- static/fetchData.js
+++ static/fetchData.js
@@ -7,6 +7,11 @@ window.addEventListener("load", () => {
     let latestPrecipitationNum = document.querySelector(".latestPrecipitationNum")
     let latestCloudNum = document.querySelector(".latestCloudNum")
 
+    let MinTempNum = document.querySelector(".MinTempNum")
+    let MaxTempNum = document.querySelector(".MaxTempNum")
+    let TotalPrecipNum = document.querySelector(".TotalPrecipNum")
+    let AvgWindNum = document.querySelector(".AvgWindNum")
+
 
     const forecastApi = 'http://localhost:8080/forecast'
     const dataApi = 'http://localhost:8080/data'
@@ -15,54 +20,58 @@ window.addEventListener("load", () => {
         return response.json()
     }).then(data => {
 
-        let last1DayTempHorsens = getSpecificData(data, "temperature", "Horsens", 1);
-        let tempH = last1DayTempHorsens.map(el => parseFloat(el.value));
-        //let tempH = last1DayTempHorsens.map(el => parseFloat(el.value));
-        let latestTempHorsens = Object.values(tempH)[Object.values(tempH).length - 1]; //returns the last/latest value
+        //-----------------------------------------------------------------------------------------------
+        //-----------------------------------Latest measurment of each kind------------------------------
+        let last1DayTempHorsens = getData(data, "temperature", "Horsens");
+        let latestTempHorsens = Object.values(last1DayTempHorsens)[Object.values(last1DayTempHorsens).length - 1];
+        const LatestTemperatureData = new Temperature(latestTempHorsens.time, latestTempHorsens.place, latestTempHorsens.type, latestTempHorsens.unit, latestTempHorsens.value);
 
-        //const temperatureData = new Temperature("2022-09-25T22:00:00.000Z", "Horsens", "temperature", "C", tempH)
+        let last1DayWindHorsens = getData(data, "wind speed", "Horsens");
+        let latestWindHorsens = Object.values(last1DayWindHorsens)[Object.values(last1DayWindHorsens).length - 1];
+        const LatestWindData = new Wind(latestWindHorsens.time, latestWindHorsens.place, latestWindHorsens.type, latestWindHorsens.unit, latestWindHorsens.value, latestWindHorsens.direction);
+
+        let last1DayPrecipitationHorsens = getData(data, "precipitation", "Horsens");
+        let latestPrecipHorsens = Object.values(last1DayPrecipitationHorsens)[Object.values(last1DayPrecipitationHorsens).length - 1];
+        const LatestPrecipData = new Precipitation(latestPrecipHorsens.time, latestPrecipHorsens.place, latestPrecipHorsens.type, latestPrecipHorsens.unit, latestPrecipHorsens.value, latestPrecipHorsens.precipitationType);
+
+        let last1DayCloudHorsens = getData(data, "cloud coverage", "Horsens");
+        let latestCloudHorsens = Object.values(last1DayCloudHorsens)[Object.values(last1DayCloudHorsens).length - 1];
+        const LatestCloudData = new CloudCoverage(latestCloudHorsens.time, latestCloudHorsens.place, latestCloudHorsens.type, latestCloudHorsens.unit, latestCloudHorsens.value);
         
-        //const temperatureData = new Temperature();
+        // let last1DayTempHorsens = getData(data, "temperature", "Horsens");
+        // let tempH = last1DayTempHorsens.map(n => parseFloat(n.value));
+        // let latestTempHorsens = Object.values(tempH)[Object.values(tempH).length - 1];
 
-        //testing new helper method
-        let testing = getData(data, "temperature", "Horsens");
-        let testingH = testing.map(el => parseFloat(el.value));
-        let testigLatest = Object.values(testingH)[Object.values(testingH).length - 1];
-        console.log("returns latest value: " + testigLatest)
+        // let last1DayWindHorsens = getData(data, "wind speed", "Horsens");
+        // let windH = last1DayWindHorsens.map(n => parseFloat(n.value));
+        // let latestWindHorsens = Object.values(windH)[Object.values(windH).length - 1];
+
+        // let last1DayPrecipitationHorsens = getData(data, "precipitation", "Horsens");
+        // let precipH = last1DayPrecipitationHorsens.map(n => parseFloat(n.value));
+        // let latestPrecipHorsens = Object.values(precipH)[Object.values(precipH).length - 1];
+
+        // let last1DayCloudHorsens = getData(data, "cloud coverage", "Horsens");
+        // let cloudH = last1DayCloudHorsens.map(n => parseFloat(n.value));
+        // let latestCloudHorsens = Object.values(cloudH)[Object.values(cloudH).length - 1];
+
+
+        //------------------------------------------------------------------------------------------------
+
+        // console.log("returns last days temperature objects(24): ")
+        // for(let i=1;i<=25;i++){
+        //     console.log("obj")
+        //     const testingobj = Object.values(last1DayTempHorsens)[Object.values(last1DayTempHorsens).length - i];
+        //     console.log(testingobj)
+        //     const temperatureData = new Temperature(testingobj.time, testingobj.place, testingobj.type, testingobj.unit, testingobj.value)
         
-        console.log("returns last days temperature objects(24): ")
-        for(let i=1;i<=25;i++){
-            console.log("obj")
-            const testingobj = Object.values(testing)[Object.values(testing).length - i];
-            console.log(testingobj)
-
-            const temperatureData = new Temperature(testingobj.time, testingobj.place, testingobj.type, testingobj.unit, testingobj.value)
-        
-            console.log("value: "+temperatureData.getValue())
-            
-
-        }
+        //     console.log("value: "+temperatureData.getValue())
+        // }
 
 
-
-
-        let last1DayWindHorsens = getSpecificData(data, "wind speed", "Horsens", 1);
-        let windH = last1DayWindHorsens.map(el => parseFloat(el.value));
-        let latestWindHorsens = Object.values(windH)[Object.values(windH).length - 1];
-
-        let last1DayPrecipitationHorsens = getSpecificData(data, "precipitation", "Horsens", 1);
-        let precipH = last1DayPrecipitationHorsens.map(el => parseFloat(el.value));
-        let latestPrecipHorsens = Object.values(precipH)[Object.values(precipH).length - 1];
-
-        let last1DayCloudHorsens = getSpecificData(data, "cloud coverage", "Horsens", 1);
-        let cloudH = last1DayCloudHorsens.map(el => parseFloat(el.value));
-        let latestCloudHorsens = Object.values(cloudH)[Object.values(cloudH).length - 1];
-
-
-        latestTemperatureNum.textContent = "Latest temperature: " + latestTempHorsens + "°C";
-        latestWindNum.textContent = "Latest wind speed: " + latestWindHorsens + "m/s";
-        latestPrecipitationNum.textContent = "Latest precipitation: " + latestPrecipHorsens + "mm";
-        latestCloudNum.textContent = "Latest cloud coverage: " + latestCloudHorsens + "%";
+        latestTemperatureNum.textContent = "Latest temperature: " + LatestTemperatureData.getValue() + " " + LatestTemperatureData.getUnit();
+        latestWindNum.textContent = "Latest wind speed: " + LatestWindData.getValue() + " " + LatestWindData.getUnit();
+        latestPrecipitationNum.textContent = "Latest precipitation: " + LatestPrecipData.getValue() + " " + LatestPrecipData.getUnit();
+        latestCloudNum.textContent = "Latest cloud coverage: " + LatestCloudData.getValue() + " " + LatestCloudData.getUnit();
 
 
 
